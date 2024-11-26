d17df2f3e44325e1caf7a3f4d97cdf5d664e058f
Urbaeva <burulaiurbaeva7@gmail.com>
2024-11-26 11:31:09 +0600

    initial commit

---
+++ lib/data/questions.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_section3/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What are the main building blocks of Flutter UIs?',
+    [
+      'Widgets',
+      'Components',
+      'Blocks',
+      'Functions',
+    ],
+  ),
+  QuizQuestion(
+    'Which programming language is primarily used for Flutter development?',
+    [
+      'Dart',
+      'JavaScript',
+      'Python',
+      'Java',
+    ],
+  ),
+  QuizQuestion(
+    'What is the purpose of a StatefulWidget in Flutter?',
+    [
+      'To create a widget that can rebuild when state changes',
+      'To create a widget that does not rebuild',
+      'To define the UI structure of an app',
+      'To handle network requests',
+    ],
+  ),
+  QuizQuestion(
+    'Which widget is commonly used to display a list of items in Flutter?',
+    [
+      'ListView',
+      'Column',
+      'Row',
+      'Container',
+    ],
+  ),
+  QuizQuestion(
+    'What is the name of the tool used to manage dependencies in a Flutter project?',
+    [
+      'pubspec.yaml',
+      'package.json',
+      'build.gradle',
+      'dependencies.xml',
+    ],
+  ),
+];
